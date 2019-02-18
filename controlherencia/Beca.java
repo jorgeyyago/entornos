30de986a8bbbb13490ca96821cb2834635a25577
casa <casa@DESKTOP-VVBRUBU>
2019-02-18 19:31:06 +0100

    cuarto commit para modificar la clase becas

--- controlherencia/Beca.java
+++ controlherencia/Beca.java
@@ -13,4 +13,34 @@ public class Beca {
 		this.dnialumno = dnialumno;
 		this.renta = renta;
 	}
+	public int darPuntuacion() {
+		if (ciudadDeOrigen.equalsIgnoreCase("madrid"))
+			puntuacion=puntuacion+1;
+		if(renta < 6000)
+			puntuacion=puntuacion+2;
+		if(renta >= 6000 && renta <= 10000)
+			puntuacion=puntuacion+1;
+		return puntuacion;
+	}
+
+
+
+
+
+	public int getNumfp() {
+		return numfp;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Fp [numfp=" + numfp + ", ciudadDeOrigen=" + ciudadDeOrigen + ", codigo=" + codigo + ", toString()="
+				+ super.toString() + "]";
+	}
+
+
+
+
+
 
